eff00022aed35dedfe9d3433de403d7984bb4bc5
Mayara Stresser <mstresses@hotmail.com>
2020-02-17 17:05:24 -0300

    atividade aula

---
+++ MVCWebPresentationLayer/Controllers/ClienteController.cs
@@ -0,0 +1,50 @@
+﻿using MVCWebPresentationLayer.Mock;
+using MVCWebPresentationLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebPresentationLayer.Controllers
+{
+    public class ClienteController : Controller
+    {
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        //Precisa condecorar ele, e significa que vai receber dados.
+        [HttpPost]
+        public ActionResult Cadastrar(CadastroClienteViewModel clienteViewModel)
+        {
+            ClienteMockBLL mockBLL = new ClienteMockBLL();
+            try
+            {
+                //Esta linha pode lançar uma exception, logo ela deve estar dentro e um bloco try.
+                mockBLL.Cadastrar(clienteViewModel);
+
+                //Se chegou aqui, deu boa, não deu exception! Redirecione o usuáro para a página inicial.
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                //Se chegou aqui, o método Cadastrar do MockBLL deu erro :(
+
+                ViewBag.MensagemErro = ex.Message;
+
+                //Retorna a mesma tela que o usuário estava
+                return View();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCWebPresentationLayer/Mock/ClienteMockBLL.cs
@@ -0,0 +1,21 @@
+﻿using MVCWebPresentationLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebPresentationLayer.Mock
+{
+    public class ClienteMockBLL
+    {
+        public void Cadastrar(CadastroClienteViewModel clienteViewModel)
+        {
+            Random rdm = new Random();
+            int numeroSorteado = rdm.Next(0, 11);
+            if (numeroSorteado < 5)
+            {
+                throw new Exception("Erro no cadastro de clientes.");
+            }
+        }
+    }
+}
